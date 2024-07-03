5bf2c20ba0b098c59837611a416e9b13e94052b2
jordanoboyle <jmcmcode@gmail.com>
2024-07-03 16:45:14 -0600

    destroy action experiences

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -26,10 +26,28 @@ class ExperiencesController < ApplicationController
   end
 
   def update
-    render json: {message: "hello there"}
+    @experience = Experience.find_by(id: 9)
+    @experience.student_id = params[:student_id] || @experience.student_id
+    @experience.company_name = params[:company_name] || @experience.company_name
+    @experience.job_title = params[:job_title] || @experience.job_title
+    @experience.start_date = params[:start_date] || @experience.start_date
+    @experience.end_date = params[:end_date] || @experience.end_date
+    @experience.description = params[:description] || @experience.description
+    
+    if @experience.save
+      render template: "experiences/show"
+    else
+      render json: {ERRORS: @expriences.errors.full_messages}
+    end
   end
 
   def destroy
-    render json: {message: "hello there"}
+    @experience = Experience.find_by(id: params[:id])
+
+    if @experience.destroy
+      render json: {message: "Experience has been removed"}
+    else
+      render json: {ERRORS: @experience.errors.full_messages}
+    end
   end
 end
