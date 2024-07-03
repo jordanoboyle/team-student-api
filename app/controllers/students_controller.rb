bc441532f9eb0af69e3b189758eb937d964b570a
jordanoboyle <jmcmcode@gmail.com>
2024-07-03 13:01:03 -0600

    create action student, update action student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,23 +2,57 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
-    render template: "students#index"
+    render template: "students/index"
   end
 
   def show
     @student = Student.find_by(id: params[:id])
-    render :show
+    render template: "students/show"
   end
 
   def create
-    render json: {messges: "Hello there!"}
+    @student = Student.new()
+    @student.first_name = params[:first_name] 
+    @student.last_name = params[:last_name] 
+    @student.email = params[:email] 
+    @student.phone_number = params[:phone_number] 
+    @student.bio = params[:bio] 
+    @student.linked_in_url = params[:linked_in_url] 
+    @student.twitter_handle = params[:twitter_handle] 
+    @student.blog_url = params[:blog_url] 
+    @student.resume_url = params[:resume_url] 
+    @student.github_url = params[:github_url] 
+    @student.image = params[:image] 
+
+    if @student.save
+      render template: "students/show"
+    else
+      render json: {ERRORS: @students.errors.full_messages}
+    end
+
   end
 
-  def updated
-    render json: {messges: "Hello there!"}
+  def update
+    @student = Student.find_by(id: params[:id])
+    @student.first_name = params[:first_name]  || @student.first_name
+    @student.last_name = params[:last_name] ||  @student.last_name
+    @student.email = params[:email] ||  @student.email
+    @student.phone_number = params[:phone_number] ||  @student.phone_number
+    @student.bio = params[:bio] ||  @student.bio
+    @student.linked_in_url = params[:linked_in_url] ||  @student.linked_in_url
+    @student.twitter_handle = params[:twitter_handle] ||  @student.twitter_handle
+    @student.blog_url = params[:blog_url] ||  @student.blog_url
+    @student.resume_url = params[:resume_url] ||  @student.resume_url
+    @student.github_url = params[:github_url] ||  @student.github_url
+    @student.image = params[:image] ||  @student.image
+    
+    @student.save 
+    render template: "students/show"
   end
 
   def destroy
-    render json: {messges: "Hello there!"}
+    # @student = Student.find_by(id: params[:id])
+
+    # if 
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   get "/students" => "students#index"
   get "/students/:id" => "students#show"
   post "/students"  => "students#create"
-  patch "/students/:id" => "students#updates"
+  patch "/students/:id" => "students#update"
   delete "students/:id" => "students#destroy"
 end
