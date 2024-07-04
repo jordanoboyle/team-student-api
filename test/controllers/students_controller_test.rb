0bb9a02c761b5fb2abc5a9b6e9c37c4a68dc5bdf
jordanoboyle <jmcmcode@gmail.com>
2024-07-03 21:16:50 -0600

    student create with pw/credentials, test

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -4,4 +4,11 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "create" do
+    assert_difference "Student.count", 1 do
+      post "/students.json", params: { first_name: "Test", last_name: "Test", email: "test@test.com", password: "password", password_confirmation: "password" }
+      assert_response 201
+    end
+  end
+
 end
