55a4e8cfaec3e3207ceb86822993f642c99ea80f
jordanoboyle <jmcmcode@gmail.com>
2024-07-03 22:04:46 -0600

    index, show, update, destroy tests pass student controller

--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -11,4 +11,50 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "index" do
+    get "/students.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal Student.count, data.length
+  end
+
+  test "show" do
+    get "/students/#{Student.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id",
+    "first_name", 
+    "last_name", 
+    "email", 
+    # "password_digest", 
+    "phone_number",
+    "bio",
+    "linked_in_url",
+    "twitter_handle",
+    "blog_url",
+    "resume_url",
+    "github_url",
+    "image", 
+    "created_at", 
+    "updated_at",
+    "experiences"], data.keys
+  end
+
+  test "update" do
+    student = Student.first
+    patch "/students/#{student.id}.json", params: { first_name: "Updated name" }
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal "Updated name", data["first_name"]
+  end
+
+  test "destroy" do
+    assert_difference "Student.count", -1 do
+      delete "/students/#{Student.first.id}.json"
+      assert_response 200
+    end
+  end
 end
